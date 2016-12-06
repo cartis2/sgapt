8711e8eac597f7132b5271958e39e98dd9832ce1
cartis2 <dkdkdk2zerg@naver.com>
2016-12-06 13:48:27 +0900

    UI 추가 테스트 UI생성

--- PT/3D/3D/cMainGame.h
+++ PT/3D/3D/cMainGame.h
@@ -29,6 +29,48 @@ private:
 
 	cBoundBox* Box;
 
+	//UI TEST
+	LPD3DXSPRITE				m_pSprite;
+	D3DXIMAGE_INFO				m_stImageInfo;
+	LPDIRECT3DTEXTURE9			m_pTexture;
+	LPD3DXFONT					m_pFont;
+
+	LPD3DXSPRITE				m_pSprite2;
+	LPDIRECT3DTEXTURE9			m_pTexture2;
+	D3DXIMAGE_INFO				m_stImageInfo2;
+
+	//수락
+	LPD3DXSPRITE				m_pSprite3;
+	LPDIRECT3DTEXTURE9			m_pTexture3;
+	D3DXIMAGE_INFO				m_stImageInfo3;
+	LPD3DXFONT					m_pFont3;
+
+	//거절
+	LPD3DXSPRITE				m_pSprite4;
+	LPDIRECT3DTEXTURE9			m_pTexture4;
+	D3DXIMAGE_INFO				m_stImageInfo4;
+	LPD3DXFONT					m_pFont4;
+	POINT pt;
+	POINT ptS;
+	bool m_close;
+	float pos_x;
+	float pos_y;
+	int m_R;
+	int m_R2;
+	bool m_isSel;
+	std::string s;
+	float save_x;
+	int sel;
+	bool agree;
+	bool cancle;
+	int push_i;
+	//마우스
+	POINT					m_ptPrevMouse;
+	bool					m_isLButtonDown;
+	bool					m_move;
+	float					m_fAngleX;
+	float					m_fAngleY;
+
 public:
 	cMainGame(void);
 	~cMainGame(void);
