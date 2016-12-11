9d0357a6b7f7a1b2fe0457cea2e9a29b65860856
cartis2 <dkdkdk2zerg@naver.com>
2016-12-12 02:12:00 +0900

    사라진 폴더 수정

---
+++ PT_LSH/통합test/Dx3D/cCrtController.h
@@ -0,0 +1,32 @@
+#pragma once
+
+class iMap;
+class cCrtController
+{
+private:
+	float						m_fSpeed;
+	float						m_fAngle;
+	D3DXVECTOR3					m_vPosition;
+	D3DXMATRIXA16				m_matWorld;
+	SYNTHESIZE_PASS_BY_REF(D3DXVECTOR3, m_vDirection, Direction);
+
+	vector<D3DXVECTOR3> m_vecVertex;
+	D3DXVECTOR3 vPosition;
+
+	ST_PC_VERTEX RayX[2];
+	ST_PC_VERTEX RayY[2];
+
+public:
+	cCrtController(void);
+	~cCrtController(void);
+
+	void Setup(vector<D3DXVECTOR3> vertex);
+	void Update();
+	bool GetHeight(IN float x, OUT float& y, IN float z);
+
+	D3DXVECTOR3* GetPosition() { return &m_vPosition; }
+	D3DXMATRIXA16* GetWorldTM() { return &m_matWorld; }
+
+	void RayRender();
+};
+
